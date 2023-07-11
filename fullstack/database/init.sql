7c33b5e851940ffd5b8667e392b6b9e718c9c176
MPacris <mpacris@yahoo.com>
2023-07-11 13:56:50 -0500

    devops: update init.sql

--- fullstack/database/init.sql
+++ fullstack/database/init.sql
@@ -1,12 +1,3 @@
 CREATE DATABASE IF NOT EXISTS greenthumb_hub_database;
 
-USE greenthumb_hub_database;
 
-CREATE TABLE IF NOT EXISTS `UserGardens` (
-    id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    garden_id INTEGER NOT NULL,
-    PRIMARY KEY (id, user_id, garden_id),
-    FOREIGN KEY(garden_id) REFERENCES garden (id),
-    FOREIGN KEY(user_id) REFERENCES user (id)
-);
